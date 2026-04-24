977a5f2e5e1075a0af332c9ecb41acc7eb251e68
Lautaro Sardina <Asardina@irsacorp.com.ar>
2026-04-24 18:31:14 -0300

    setee para que la app sea pwa, sigue en proceso, voy a deployar la página, porque al parecer detecta mejor pwa si esta deployada

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/global.css";
+import { registerSW } from 'virtual:pwa-register'
 
+if ('serviceWorker' in navigator) {
+  registerSW({ immediate: true })
+}
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
